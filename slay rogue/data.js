c106fa5a9cfe56a532c72e70a2f0a86672ddc04d
seppnt <yuriasantos@hotmail.com.br>
2024-06-17 21:44:13 -0300

    update

---
+++ slay rogue/data.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById("breakoutCanvas");
+    canvas.width = 300
+    canvas.height =500
+    const ctx = canvas.getContext("2d");
+
+let hand=["strike","strike","block"]
+let handPos=[50, 110, 170]
+
+
+
+
+let texto = ""
