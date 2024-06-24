65fdd7fff548233c19dfab84ef045c2590df0a2d
yuriasantos@hotmail.com.br <yuriasantos@hotmail.com.br>
2024-06-24 12:39:47 -0300

    update main.js

--- tatical board/main.js
+++ tatical board/main.js
@@ -4,6 +4,8 @@ const ctx = canvas.getContext("2d");
 
 let grid = [[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 
+let p1={x:0, y:0}
+
 function updateUi() {
   for (let i in grid) {
     for (let j in grid[i]) {
@@ -19,6 +21,10 @@ function updateUi() {
       ctx.strokeRect(50 * i, 50 * j, 50, 50)
     }
   }
+  ctx.fillStyle = "black"
+  ctx.fillRect((p1.x*50)+15, (p1.y*50)+15, 20, 20)
+  
+  
 }
 
 function update() {
@@ -36,6 +42,9 @@ canvas.addEventListener('touchstart', function(e) {
   const touchY = e.touches[0].clientY - canvas.offsetTop;
   let x = Math.trunc(touchX / 50)
   let y = Math.trunc(touchY / 50)
-grid[x][y] = 1
+grid[p1.x][p1.y]=0
+p1.x=x
+p1.y=y
+grid[p1.x][p1.y]=1
 
 });
