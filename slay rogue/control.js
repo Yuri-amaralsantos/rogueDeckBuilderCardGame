736ed27c77e598f7869fa2fa66d9f1dfd197a3ed
seppnt <yuriasantos@hotmail.com.br>
2024-08-07 11:22:51 -0300

    update control.js

--- slay rogue/control.js
+++ slay rogue/control.js
@@ -1,10 +1,15 @@
+import {player} from "./player.js"
+
+
+
 export function controle (e) {
   const touchX = e.touches[0].clientX - canvas.offsetLeft;
   const touchY = e.touches[0].clientY - canvas.offsetTop;
-  /*
-  for (let i in hand) {
-    if (touchX >= handPos[i] && touchX <= handPos[i] + 58 &&
+  for (let i in player.hand) {
+    if (touchX >= 10+(58*i) && touchX <= (10+(58*i))+58 &&
       touchY >= 490 && touchY <= 490 + 80) {
+     console.log(i)
+     /*
       if (p1.mana >= hand[i].cost) {
         playEffect(p1, bots[p1.target], hand[i])
         p1.mana -= hand[i].cost
@@ -12,8 +17,9 @@ export function controle (e) {
         hand.splice(i, 1)
       }
       break
-
+*/
     }
+    /*
     for (let i in bots) {
       if (touchX >= botPos[i] && touchX <= botPos[i] + 60 &&
         touchY >= 90 && touchY <= 90 + 30) {
@@ -21,8 +27,8 @@ export function controle (e) {
         break
       }
     }
+    */
   }
-  */
   if (touchX >= 310 && touchX <= 310 + 60 &&
     touchY >= 230 && touchY <= 230 + 30) {
     //pass()
