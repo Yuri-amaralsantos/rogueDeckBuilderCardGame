20da7debd182a70c4a95d4434a91fcd033fedfba
seppnt <yuriasantos@hotmail.com.br>
2024-08-07 11:55:25 -0300

    update

--- slay rogue/enemy.js
+++ slay rogue/enemy.js
@@ -1,4 +1,4 @@
-export class Bot {
+class Bot {
   constructor(name, acts) {
     this.name = name
     this.actionList = acts
@@ -9,18 +9,19 @@ export class Bot {
   weak = 0
 
   action = ""
+  /*
   choose = function() {
     let r = rng(0, this.actionList.length - 1)
     this.action = this.actionList[r]
   }
+  */
 }
 
-let mob1 = new Bot("mob1", [a1, a2, a4])
-let mob2 = new Bot("mob2", [a1, a2, a3])
+//let mob1 = new Bot("mob1", [a1, a2, a4])
+//let mob2 = new Bot("mob2", [a1, a2, a3])
 
-
+let mob1 = new Bot("mob1")
+let mob2 = new Bot("mob2")
 
 export let bots = [{ ...mob1 }, { ...mob1 }, { ...mob2 }]
 
-bots[0].choose()
-let botPos = [50, 160, 270]
--- slay rogue/interface.js
+++ slay rogue/interface.js
@@ -1,5 +1,6 @@
-import {player} from "./player.js"
-import {game} from "./data.js"
+import { player } from "./player.js"
+import { bots } from "./enemy.js"
+import { game } from "./data.js"
 
 const canvas = document.getElementById("canvas");
 canvas.width = 370
@@ -14,48 +15,45 @@ export function updateUi() {
     ctx.fillStyle = "green"
     ctx.strokeStyle = '#000';
     ctx.lineWidth = 2;
-    ctx.fillRect(10+(58*i), 490, 58, 80)
-    ctx.strokeRect(10+(58*i), 490, 58, 80);
+    ctx.fillRect(10 + (58 * i), 490, 58, 80)
+    ctx.strokeRect(10 + (58 * i), 490, 58, 80);
 
     ctx.font = '10px Arial';
     ctx.fillStyle = 'white';
-    ctx.fillText(player.hand[i].cost, 20+(58*i), 510)
-    ctx.fillText(player.hand[i].t1, 20+(58*i), 530)
-    if(player.hand[i].t2){
-       ctx.fillText(player.hand[i].t2, 20+(58*i), 550)
+    ctx.fillText(player.hand[i].cost, 20 + (58 * i), 510)
+    ctx.fillText(player.hand[i].t1, 20 + (58 * i), 530)
+    if (player.hand[i].t2) {
+      ctx.fillText(player.hand[i].t2, 20 + (58 * i), 550)
     }
 
   }
-/*
   for (let i in bots) {
-    ctx.beginPath();
-    ctx.rect(botPos[i], 90, 60, 30);
-    if (p1.target == i) {
+    if (player.target == i) {
       ctx.fillStyle = "red"
     } else {
       ctx.fillStyle = "yellow"
     }
-    ctx.fill();
-    ctx.closePath();
+    ctx.fillRect(50 + (110 * i), 90, 60, 30)
+
 
     ctx.font = '14px Arial';
     ctx.fillStyle = 'black';
-    ctx.fillText(bots[i].name, botPos[i] + 10, 90)
-    ctx.fillText("hp:" + bots[i].hp +"+"+bots[i].armor, botPos[i] + 10, 130)
-    ctx.fillText(bots[i].action.t1, botPos[i] + 10, 150)
+    ctx.fillText(bots[i].name, 50 + (110 * i), 90)
+    ctx.fillText("hp:" + bots[i].hp + "+" + bots[i].armor, 50 + (110 * i), 130)
+    //ctx.fillText(bots[i].action.t1, botPos[i] + 10, 150)
   }
-*/
+  */
   //pass
   ctx.fillStyle = "orange"
   ctx.fillRect(310, 230, 60, 30);
   ctx.font = '14px Arial';
   ctx.fillStyle = 'black'
   ctx.fillText("pass", 325, 250)
-  
+
   ctx.fillText("turn:" + game.turn, 30, 400)
-  
+
   //hp player
-  ctx.fillText("player hp:" + player.hp+"+"+player.armor, 30, 450)
+  ctx.fillText("player hp:" + player.hp + "+" + player.armor, 30, 450)
   ctx.fillText("mana: " + player.mana, 30, 470)
 
 
